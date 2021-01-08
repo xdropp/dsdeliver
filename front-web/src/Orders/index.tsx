8f6443e5108b59cc32622e5074734c4f13fea612
unknown <gabriel_froes@hotmail.com>
2021-01-08 01:23:49 -0300

    Front-end 50%

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from './api';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders(){
+    const [products, setProducts] = useState<Product[]>([]);
+    
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => window.alert('Erro ao listar produtos!!!'))
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products}/>
         </div>
     )
 }
